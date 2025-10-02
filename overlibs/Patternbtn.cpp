11767c87fa1db61e242bcf7d827a8589a86e5000
dequar <thisdequar@gmail.com>
2025-10-02 21:01:11 +0300

    improv

--- overlibs/Patternbtn.cpp
+++ overlibs/Patternbtn.cpp
@@ -2,6 +2,7 @@
 #include <pico/time.h>
 #include <hardware/gpio.h>
 #include "Minory.cpp"
+#include "allpins.h"
 
 bool offon_pattern(bool pressed1, bool pressed2, bool &running, bool &enabled, absolute_time_t &press_start) {
     if (pressed1 && pressed2) {
