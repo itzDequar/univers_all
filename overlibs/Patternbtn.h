6d06b019fefcc63586c4f8e5b502635c8a5d2229
dequar <thisdequar@gmail.com>
2025-10-04 11:06:02 +0300

    improv

--- overlibs/Patternbtn.h
+++ overlibs/Patternbtn.h
@@ -6,4 +6,4 @@
 #include "allpins.h"
 
 bool offon_pattern(bool pressed1, bool pressed2, bool &running, bool &enabled, absolute_time_t &press_start);
-bool pattern_button(bool pressed, absolute_time_t &press_start);
+bool any_pattern(bool pressed, absolute_time_t &press_start);
